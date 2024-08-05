6917b578362076f5a34e5ad41a983d2c6d9a30f3
Rodika Chorba <158561902+RodikaChorba@users.noreply.github.com>
2024-08-05 22:06:40 +0200

    deploi change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/prop-types */
 import css from "./Contact.module.css";
+import { IoMdPerson } from "react-icons/io";
+import { FaPhone } from "react-icons/fa";
 
 const Contact = ({ name, number, onDeleteContact, id }) => {
   return (
     <li className={css.item}>
       <div className={css.userData}>
-        <p className={css.line}>{name}</p>
-        <p className={css.line}>{number}</p>
+        <p className={css.line}>
+          <IoMdPerson />
+          {name}
+        </p>
+        <p className={css.line}>
+          <FaPhone />
+          {number}
+        </p>
       </div>
       <button
         className={css.button}
